e755a82bb231e6716dd99dd771faa8826e23a1d3
Liam <liam.blackburn@madetech.com>
2025-12-11 15:09:15 +0000

    refactored tests for newly implemented logic

--- tests/test_uprn_to_council.py
+++ tests/test_uprn_to_council.py
@@ -1,26 +1,20 @@
-# from src.council_data_utils import get_council_code_for_uprn
-# import pytest
+from src.council_data_utils import load_uprn_to_council
+import pytest
 
-# @pytest.mark.parametrize(
-#         "uprn, expected_council_code",
-#         [
-#             ("100061342030", "E07000207"),
-#             ("100061342031", "E07000207"),
-#             ("10033322698", "E07000207")
-#         ]
-# )
-# def test_get_council_code_for_uprn_returns_correct_for_valid_uprn(uprn, expected_council_code):
-#     council_code = get_council_code_for_uprn(uprn)
-#     assert council_code == expected_council_code
+@pytest.mark.parametrize(
+        "uprn, expected_council_code",
+        [
+            ("100061342030", "E07000207"),
+            ("100061342031", "E07000207"),
+            ("10033322698", "E07000207"),
+            ("10015441587","E06000023")
+        ]
+)
+def test_load_uprn_to_council_returns_correct_for_valid_uprn(uprn, expected_council_code):
+    uprn_to_council_dict = load_uprn_to_council(expected_council_code)
+    assert uprn_to_council_dict.get(uprn) == expected_council_code
 
-# def test_get_council_code_for_uprn_returns_None_for_invalid_uprn():
-#     uprn = "this is not a uprn"
 
-#     council_code = get_council_code_for_uprn(uprn)
-#     assert council_code is None
-
-# def test_get_council_code_for_uprn_returns_false_for_None():
-#     uprn = None
-
-#     council_code = get_council_code_for_uprn(uprn)
-#     assert council_code is None
+def test_load_uprn_to_council_returns_false_for_None():
+    uprn_to_council_dict = load_uprn_to_council("E06000023")
+    assert uprn_to_council_dict.get(None) is None
