40510ff816287cc7995686e135cef85f3ea6563e
Liam <liam.blackburn@madetech.com>
2025-12-11 15:22:09 +0000

    amend test suite for none and empty dict keys

--- tests/test_uprn_to_council.py
+++ tests/test_uprn_to_council.py
@@ -15,6 +15,7 @@ def test_load_uprn_to_council_returns_correct_for_valid_uprn(uprn, expected_coun
     assert uprn_to_council_dict.get(uprn) == expected_council_code
 
 
-def test_load_uprn_to_council_returns_false_for_None():
+def test_load_uprn_to_council_has_no_empty_or_none_keys():
     uprn_to_council_dict = load_uprn_to_council("E06000023")
-    assert uprn_to_council_dict.get(None) is None
+    assert None not in uprn_to_council_dict
+    assert "" not in uprn_to_council_dict
