8369e49dfc114693927190821ae12d5d959e537d
Joe Rawnsley <joe.rawnsley@madetech.com>
2026-01-07 15:00:14 +0000

    update Leaflet styling to colour polygons based on epc_score and grey if no epc_score

--- static/javascripts/hard_to_heat_map.js
+++ static/javascripts/hard_to_heat_map.js
@@ -13,25 +13,45 @@ function initMap() {
         .then((response) => response.json())
         .then((data) => {
             L.geoJSON(data, {
-                style: function(feature) {
-                    switch (feature.properties.average_epc_score) {
-                        case 61: 
-                            return {
-                                color: "#1bb558",
-                                weight: 2,
-                            }
-                        case 70: 
-                            return {
-                                color: "#fcaa64",
-                                weight: 2,
-                            }
-                        case null: 
-                            return {
-                                color: "#9b9b9bd0",
-                                weight: 2,
-                            }
-                        }
-                    },
+                style: function (feature) {
+                    const epc_score = Number(feature.properties.epc_score);
+                    console.log("epc score is.... ", epc_score);
+                    if (isNaN(epc_score))
+                        return {
+                            color: "#c1c1c1ff",
+                            weight: 2,
+                        };
+
+                    if (epc_score >= 80) {
+                        console.log("score is 80+");
+                        return {
+                            color: "#00ff95ff",
+                            weight: 2,
+                        };
+                    }
+                    if (epc_score >= 60) {
+                        console.log("score is 60+");
+                        return {
+                            color: "#d4ff00ff",
+                            weight: 2,
+                        };
+                    }
+                    if (epc_score >= 40) {
+                        console.log("score is 40+");
+                        return {
+                            color: "#ffbb00ff",
+                            weight: 2,
+                        };
+                    }
+                    if (epc_score < 40) {
+                        console.log("score is under 40");
+                        return {
+                            color: "#ff0000ff",
+                            weight: 2,
+                        };
+                    }
+                },
+
                 onEachFeature: function (feature, layer) {
                     epc_score = feature.properties.average_epc_score
                         ? feature.properties.average_epc_score
