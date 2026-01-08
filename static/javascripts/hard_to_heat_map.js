94359042a72b7b49a2d5045c20997ed3fdd27c6f
Liam <liam.blackburn@madetech.com>
2026-01-08 12:03:45 +0000

    replaced fallback heat score with neutral icon and no score

--- static/javascripts/hard_to_heat_map.js
+++ static/javascripts/hard_to_heat_map.js
@@ -94,6 +94,16 @@ function initMap() {
             popupAnchor: [1, -34],
             shadowSize: [41, 41],
         }),
+        5: L.icon({
+            iconUrl: 
+                "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-grey.png",
+            shadowUrl: 
+                "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+            iconSize: [25, 41],
+            iconAnchor: [12, 41],
+            popupAnchor: [1, -34],
+            shadowSize: [41, 41],
+        })
     };
 
     const scoreIconsHover = {
@@ -137,26 +147,36 @@ function initMap() {
             popupAnchor: [1, -34],
             shadowSize: [56, 56],
         }),
+        5: L.icon({
+            iconUrl: 
+                "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-grey.png",
+            shadowUrl: 
+                "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+            iconSize: [30, 51],
+            iconAnchor: [17, 56],
+            popupAnchor: [1, -34],
+            shadowSize: [56, 56],
+        })
     };
 
     const markersPositions = [];
 
     for (const prop of mapData.props) {
 
-        const score = Number.isInteger(prop.score) ? prop.score : null;
+        const score = Number(prop.score) >=1 && Number(prop.score) <= 4 ? Number(prop.score) : null;
 
         const marker = L.marker([prop.lat, prop.long], {
-            icon: score ? scoreIcons[score] : scoreIcons[1],
+            icon: score !== null ? scoreIcons[score] : scoreIcons[5],
         }).addTo(map);
 
         markersDict[prop.uprn] = marker;
 
         marker.on("mouseover", function () {
-            if (score) marker.setIcon(scoreIconsHover[prop.score]);
+            if (score !== null) marker.setIcon(scoreIconsHover[score]);
         });
 
         marker.on("mouseout", function () {
-           if (score) marker.setIcon(scoreIcons[prop.score]);
+           if (score !== null) marker.setIcon(scoreIcons[score]);
         });
 
         marker.on("click", async () => {
