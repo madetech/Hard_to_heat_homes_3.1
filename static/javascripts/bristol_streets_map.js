9849381ba5766b58a70c06911138f8c8913c932e
Ryan Murray <ryan.murray@madetech.com>
2026-01-15 11:18:43 +0000

    update scoring logic conditional statement and default map zoom level

--- static/javascripts/bristol_streets_map.js
+++ static/javascripts/bristol_streets_map.js
@@ -3,7 +3,7 @@ let streetLayer;
 
 function initMap() {
     // Initialize the Leaflet map
-    map = L.map('leaflet-container').setView([51.454, -2.5879], 13) // Center on Bristol
+    map = L.map('leaflet-container').setView([51.463907, -2.584353], 17) // Center on Bristol
 
     // Add basic OpenstreetMap tile layer
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -15,12 +15,62 @@ function initMap() {
         .then(data => {
             // Create the street layer
             streetLayer = L.geoJSON(data, {
-                style: {
-                    color: "orange",
-                    weight: 1,
-                    fillColor: "yellow",
-                    fillOpacity: 0.5
-                }
+                style: function (feature) {
+                    const epc_score = Number(feature.properties.epc_score);
+                    console.log("epc score is.... ", epc_score);
+                    if (isNaN(epc_score))
+                        return {
+                            color: "#c1c1c1ff",
+                            weight: 2,
+                        };
+
+                    if (epc_score >= 80) {
+                        console.log("score is 80+");
+                        return {
+                            color: "#00ff95ff",
+                            weight: 2,
+                        };
+                    }
+                    if (epc_score >= 60) {
+                        console.log("score is 60+");
+                        return {
+                            color: "#d4ff00ff",
+                            weight: 2,
+                        };
+                    }
+                    if (epc_score >= 40) {
+                        console.log("score is 40+");
+                        return {
+                            color: "#ffbb00ff",
+                            weight: 2,
+                        };
+                    }
+                    if (epc_score < 40) {
+                        console.log("score is under 40");
+                        return {
+                            color: "#ff0000ff",
+                            weight: 2,
+                        };
+                    }
+                },
+
+                onEachFeature: function (feature, layer) {
+                    epc_score = feature.properties.average_epc_score
+                        ? feature.properties.average_epc_score
+                        : "unknown";
+                    layer.bindPopup(
+                        "Number of buildings: 30" +
+                            "<br>" +
+                        "Address: " +
+                            feature.properties.address +
+                            "<br>" +
+                        "Postcode: " +
+                            feature.properties.postcode +
+                            "<br>" +
+                        "Average EPC Score: " +
+                            epc_score
+                    );
+                },
             }).addTo(map);
         })
         .catch(err => console.log("Error loading GeoJSON", err));
